75968ad896055b86726e094d606e8e4239c9edf6
MinhyoungJeong <minhyoung123@naver.com>
2025-10-16 15:18:14 +0900

    01

---
+++ week7/week7/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  week7
+//
+//  Created by 정민형 on 10/16/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int flag =1;
+    while(flag !=0)
+    {
+        int y;
+        y =3;
+        flag = 0;
+        y=4;
+    }
+    return 0;
+}
