284a6f809aa35fa64e006eb34fb2ecb297a4141b
MinhyoungJeong <minhyoung123@naver.com>
2025-10-16 15:50:40 +0900

    04

--- week7/week7/main.c
+++ week7/week7/main.c
@@ -3,23 +3,24 @@
 //  week7
 //
 //  Created by 정민형 on 10/16/25.
-//
-
 #include <stdio.h>
 
-int inc(int counter);
+void f(void);
 
-int main(void) {
-    
-    int i = 0;
-    printf("함수 호출 전 i = %d\n",i);
-    i= inc(i);
-    printf("함수 호출 후 i = %d\n",i);
+int main(void)
+{
+    int i;
+    for(i=0; i<5; i++)
+    {
+        f();
+    }
     return 0;
 }
 
-int inc(int counter)
-{
-    counter++;
-    return counter; //반환값을 이용해서 외부에서 사용
+void f(void){
+    int i;
+    for(i=0; i<10; i++) {
+        printf("#");
+    }
 }
+
